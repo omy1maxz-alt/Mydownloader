57016f64216ed6f41b00653b47e1da0fe0a22893
Rommy Rizal Ramadhan <omy1.maxz@gmail.com>
2025-09-26 21:24:58 +0700

    Update run_debug.sh

--- run_tests.sh
+++ run_tests.sh
@@ -4,4 +4,4 @@ SDK_DIR="/tmp/android_sdk_jules"
 export ANDROID_HOME=$SDK_DIR
 export ANDROID_SDK_ROOT=$SDK_DIR
 export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools
-./gradlew testDebugUnitTest
+./gradlew assembleDebug
